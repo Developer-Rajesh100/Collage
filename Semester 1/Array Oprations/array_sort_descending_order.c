7df547edf52abdae78f6271b3bfc6e7b8e9ae915
Rajesh Mondal <developerrajesh100@gmail.com>
2024-01-20 23:33:53 +0530

    Array sort

---
+++ Semester 1/Array Oprations/array_sort_descending_order.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    printf("Enter the size of Array : ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter the elements of Array : ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[i] < arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    printf("Sorted Array : ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
